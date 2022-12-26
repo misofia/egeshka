9244554b4eb72cd67a037bfa0be7d731a826494a
misofia <78451488.sm@gmail.com>
2022-12-26 23:10:52 +0300

    2

--- 14.py
+++ 14.py
@@ -5,3 +5,10 @@ while a != 0:
     a //= 5
 a5 = a5[::-1]
 print(a5.count('4'))'''
+#x y
+'''for x in '012345678':
+    for y in '012345678':
+        a = int('88' + x + '4' + y, 9) + int('7' + x + '44' + y, 11)
+        if a % 61 == 0:
+            print(a // 61)
+            break'''
