880694715da46a58e5b77cbdc2da9843b23461af
Student <lady.khodina@yandex.ru>
2023-02-28 15:20:48 +0300

    запрет на ход

--- теория игр №19-21.py
+++ теория игр №19-21.py
@@ -24,3 +24,37 @@ for s2 in range(1, 69 + 1):
     for m in range(1, 4 + 1):
         if f(7, s2, 0, m) == 1: # and m == 3
             print(s2, m)
+
+# Нельзя повторять предыдущий ход (Поляков 4825)
+def f(s, p, c, m):
+    if s >= 43: return c % 2 == m % 2
+    if c == m: return 0
+
+    h = []
+    if p != '+1': h += [f(s + 1, '+1', c + 1, m)]
+    if p != '+2': h += [f(s + 2, '+2', c + 1, m)]
+    if p != '*2': h += [f(s * 2, '*2', c + 1, m)]
+    return any(h) if (c + 1) % 2 == m % 2 else all(h)
+
+for s in range(1, 43):
+    for m in range(1, 5):
+        if f(s, '', 0, m) == 1:
+            print(s, m)
+            break
+
+# Поляков 4827
+def f(s, p, c, m):
+    if s >= 55: return c % 2 == m % 2
+    if c == m: return 0
+
+    h = []
+    if p != '+1': h += [f(s + 1, '+1', c + 1, m)]
+    if p != '+3': h += [f(s + 3, '+3', c + 1, m)]
+    if p != '*2': h += [f(s * 2, '*2', c + 1, m)]
+    return any(h) if (c + 1) % 2 == m % 2 else all(h)
+
+for s in range(1, 55):
+    for m in range(1, 5):
+        if f(s, '', 0, m) == 1:
+            print(s, m)
+            break
