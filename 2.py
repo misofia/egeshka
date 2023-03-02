ca9863aa42f853be9c0e10d7dba35e81839a6833
Student <sdgas@gmail.com>
2023-03-02 09:06:18 +0300

    код для гениев

--- 2.py
+++ 2.py
@@ -1,7 +1,23 @@
-print('X Y Z W')
+'''print('X Y Z W')
 for x in range(0, 2):
     for y in range(0, 2):
         for z in range(0, 2):
             for w in range(0, 2):
                 if ((x <= y) == (y <= z)) and (y or w):
-                    print(x, y, z, w)
+                    print(x, y, z, w)'''
+
+from itertools import *
+
+
+def f(x, y, z, w):
+    return not(y <= x) or (z <= w) or not z
+
+
+for a in product([0, 1], repeat = 7):
+    table = [(a[0], 0, a[1], a[2]), # 1 строка
+             (0, 1, a[3], a[4]),
+             (1, a[5], a[6], 0)]
+    if len(table) == len(set(table)):
+        for p in permutations('xyzw'):
+            if [f(**dict(zip(p, r))) for r in table] == [0, 0, 0]:
+                print(p)
