07ec85761f79f5e3e647cf39189b834a54e121d2
misofia <78451488.sm@gmail.com>
2023-03-13 22:20:30 +0300

    задачки

--- 12.py
+++ 12.py
@@ -16,3 +16,27 @@ while '18' in s or '288' in s or '3888' in s:
     else:
         s = s.replace('3888', '1')
 print(s)
+
+# При какой наименьшей длине исходной строки будет содержать наибольшее возможное число единиц?
+max_n = 0
+for n in range(200, 300):
+    s = '1' * n
+    while '1111' in s:
+        s = s.replace('1111', '22', 1)
+        s = s.replace('222', '1', 1)
+    if max_n < s.count('1'):
+        max_n = s.count('1')
+        index = n
+print(index)
+
+# Исходная строка начиналась с нуля, а далее содержала только единицы, двойки и тройки
+for x in range(50):
+    for y in range(50):
+        for z in range(50):
+            s = '0' + '1' * x + '2' * y + '3' * z
+            while '01' in s or '02' in s or '03' in s:
+                s = s.replace('01', '30', 1)
+                s = s.replace('02', '101', 1)
+                s = s.replace('03', '202', 1)
+            if s.count('1') == 15 and s.count('2') == 10 and s.count('3') == 60:
+                print(x)
