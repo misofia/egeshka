588b7d9644811e2b855f09bbe0a668ac742dc745
Student <lady.khodina@yandex.ru>
2023-03-21 16:19:59 +0300

    я гений

--- 12.py
+++ 12.py
@@ -50,7 +50,7 @@ while '>1' in s or '>2' in s or '>3' in s:
         s = s.replace('>2', '2>', 1)
     elif '>3' in s:
         s = s.replace('>3', '1>', 1)
-print(s.count('1') + s.count('2') * 2 + s.count('3') * 3)'''
+print(s.count('1') + s.count('2') * 2 + s.count('3') * 3)
 
 # Статград
 for z in range(1000):
@@ -62,10 +62,54 @@ for z in range(1000):
             s = s.replace('>2', '3>', 1)
         elif '>3' in s:
             s = s.replace('>3', '11>', 1)
-    sum = s.count('1') + s.count('2') * 2 + s.count('3') * 3
+    sum = s.count('1') + s.count('2') * 2 + s.count('3') * 3  #s1 = sum(list(map(int, s))) - сумма цифр в строке
     c = 0
     for i in range(1, sum + 1):
         if sum % i == 0 and i != 1 and i != sum:
             c += 1
     if c == 3:
         print(z)
+
+# 2 способ
+def div(x):
+    d = set()
+    for i in range(1, int(x ** 0.5) + 1):
+        if x % i == 0:
+            d.add(i)
+            d.add(x//i)
+    return sorted(d)
+
+
+for m in range(1000):
+    s = '>' + '1' * 15 + '2' * 35 + '3' * z
+    while '>1' in s or '>2' in s or '>3' in s:
+        s = s.replace('>1', '2>', 1)
+        s = s.replace('>2', '3>', 1)
+        s = s.replace('>3', '11>', 1)
+    s = s[:-1]
+    s1 = sum(list(map(int, s)))
+    c = 0
+    if len(div(int(s1))) == 5:
+        print(m)
+
+# Статград
+for x in range(20):
+    for y in range(20):
+        for z in range(20):
+            a = '0' + '1' * x + '2' * y + '3' * z + '0'
+            while '00' not in a:
+                a = a.replace('01', '210', 1)
+                a = a.replace('02', '3101', 1)
+                a = a.replace('03', '2012', 1)
+            if a.count('1') == 56 and a.count('2') == 44 and a.count('3') == 19:
+                print(x + y + z + 2)'''
+
+for x in range(50):
+    a = '0' + '1' * x + '2' * x + '0'
+    while '00' not in a:
+        a = a.replace('011', '20', 1)
+        a = a.replace('022', '10', 1)
+        a = a.replace('01', '220', 1)
+        a = a.replace('02', '110', 1)
+        if a.count('1') == 40 and a.count('2') > 50:
+            print(a.count('2'))
