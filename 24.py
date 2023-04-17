11bbe943fb6fee7783eeb747b832bbd9fd4fc014
misofia <78451488.sm@gmail.com>
2023-04-18 01:02:10 +0300

    24

--- 24.py
+++ 24.py
@@ -10,3 +10,16 @@ print(len(max(s, key = len)))
 
 # чтение, создание списка, в целочисленный вид
 # a = [int(x) for x in open('17.txt')]
+
+# длина строки
+a = open('24_demo.txt').readline()
+m = 0
+k = 1
+for i in range(len(a)):
+    if a[i] != a[i - 1]:
+        k += 1
+    else:
+        m = max(k, m)
+        k = 1
+m = max(k, m)
+print(m)
