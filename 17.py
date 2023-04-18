afa112cb33f0ae4fbafd17659999135153b50948
Student <lady.khodina@yandex.ru>
2023-04-18 11:16:13 +0300

    17

--- 17.py
+++ 17.py
@@ -22,7 +22,7 @@ for i in a:
     s = sum(list(map(int, str(i))))
     if (s % 5 == 0) and (i % 3 ** 2 != 0):
         ans.append(i)
-print(len(ans), max(ans))'''
+print(len(ans), max(ans))
 
 # 4311
 a = [int(x) for x in open('17-4.txt')]
@@ -32,3 +32,31 @@ for i in a:
         ans.append(i)
 print(max(ans), sum(ans))
 
+# 37348
+a = [int(x) for x in open('17.txt')]
+ans = []
+for i in range(len(a) - 1):
+    for j in range(i + 1, len(a)):
+        if (a[i] * a[j]) % 34 != 0:
+            ans.append(a[i] + a[j])
+print(len(ans), max(ans))
+
+# 37340
+a = [int(x) for x in open('17 (1).txt')]
+ans = []
+for i in range(len(a) - 1):
+    for j in range(i + 1, len(a)):
+        if (a[i] - a[j]) % 2 == 0 and (a[i] % 31 == 0 or a[j] % 31 == 0):
+            ans.append(a[i] + a[j])
+print(len(ans), max(ans))
+
+# 37357
+a = [int(x) for x in open('17 (2).txt')]
+ans = []
+for i in range(len(a) - 1):
+    for j in range(i + 1, len(a)):
+        if (a[i] + a[j]) % 8 == 0:
+            ans.append(a[i] + a[j])
+print(len(ans), max(ans)) # 6243772 19992'''
+
+# 37336
