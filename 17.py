b071081e637c385f118c805705539564c84dea05
misofia <78451488.sm@gmail.com>
2023-04-24 00:08:12 +0300

    тяжело

--- 17.py
+++ 17.py
@@ -40,62 +40,7 @@ for i in a:
         ans.append(i)
 print(max(ans))
 
-# Номер 4612 Поляков
-
-# 1 СПОСОБ
-
-'''a = [int(x) for x in open('17-4.txt')]
-k = 0
-m = 10001
-for i in a:
-    if i % 3 == i % 5 and (i % 31 == 0 or i % 47 == 0 or i % 53 == 0):
-        k += 1
-        m = min(m, i)
-    print(k, m)'''
-
-# 2 СПОСОБ
-
-'''a = [int(x) for x in open('17-4.txt')]
-k = 0
-m = 10001
-for i in range(len(a)):
-    if (a[i] % 3 == a[i] % 5) and (a[i] % 31 == 0 or a[i] % 47 == 0 or a[i] % 53 == 0):
-        k += 1
-        m = min(m, i)
-    print(k, m)'''
-
-# 3 СПОСОБ
-
-'''a = [int(x) for x in open('17-4.txt')]
-ans = []
-for i in range(len(a)):
-    if (a[i] % 3 == a[i] % 5) and (a[i] % 31 == 0 or a[i] % 47 == 0 or a[i] % 53 == 0):
-        ans.append(a[i])
-    print(len(ans), min(ans))'''
-
-# Номер 4316 Поляков
-
-'''a = [int(x) for x in open('17-4.txt')]
-ans = []
-for i in a:
-    s = sum(list(map(int, str(i))))
-    if (s % 5 == 0) and (i % 3**2 != 0):
-        ans.apppend(i)
-    print(len(ans), max(ans))'''
-
-# Номер 4311 Поляков
-
-'''a = [int(x) for x in open('17-4.txt')]
-k = 0
-m = -10001
-for i in range(len(a)):
-    if a[i] % 13 == 4 and a[i] % 8 == 1:
-        k += 1
-        m = max(m, i)
-    print(k, m)'''
-
 # Номер 37348 РЕШУ ЕГЭ
-
 '''a = [int(x) for x in open('17 (1).txt')]
 ans = []
 for i in range(len(a) - 1):
@@ -105,7 +50,6 @@ for i in range(len(a) - 1):
 print(len(ans), max(ans))'''
 
 # Номер 37340 РЕШУ ЕГЭ
-
 '''a = [int(x) for x in open('17 (2).txt')]
 ans = []
 for i in range(len(a) - 1):
@@ -115,7 +59,6 @@ for i in range(len(a) - 1):
 print(len(ans), max(ans))'''
 
 # Номер 37357 РЕШУ ЕГЭ
-
 '''a = [int(x) for x in open('17 (3).txt')]
 ans = []
 for i in range(len(a) - 1):
@@ -125,7 +68,6 @@ for i in range(len(a) - 1):
 print(len(ans), max(ans))'''
 
 # 48465
-
 '''a = [int(x) for x in open('17 (6).txt')]
 ans = []
 min6 = 10001
@@ -138,7 +80,6 @@ for i in range(len(a)-1):
 print(len(ans), max(ans))'''
 
 # Номер 47221 РЕШУ ЕГЭ
-
 '''a = [int(x) for x in open('17 (6).txt')]
 ans = []
 max6 = -10001
@@ -151,7 +92,6 @@ for i in range(len(a)-1):
 print(len(ans), max(ans))'''
 
 # Полкяков 4496
-
 '''a = [int(x) for x in open('17-205.txt')]
 ans = []
 for i in range(len(a) - 1):
@@ -160,7 +100,6 @@ for i in range(len(a) - 1):
 print(len(ans), max(ans))'''
 
 # Поляков 5066
-
 '''a = [int(x) for x in open('17-288.txt')]
 ans = []
 for i in range(len(a) - 2):
@@ -169,7 +108,6 @@ for i in range(len(a) - 2):
 print(len(ans), min(ans))'''
 
 # Пояков 4693
-
 '''a = [int(x) for x in open('17-243.txt')]
 ans = []
 m = []
@@ -183,7 +121,6 @@ for i in range(len(a) - 1):
 print(len(ans), min(ans))'''
 
 # Пояков 4688
-
 '''a = [int(x) for x in open('17-243.txt')]
 ans = []
 m = []
