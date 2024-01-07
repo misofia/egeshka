7cc14b65d444b0061724390e1be1edccc4407775
misofia <78451488.sm@gmail.com>
2024-01-07 17:47:27 +0300

    ура через def

--- 5.py
+++ 5.py
@@ -8,6 +8,7 @@
     if int(r2, 2) > 43:
         print(int(r2, 2))
         break'''
+
 '''for n in range(256):
     s = bin(n)[2:]  
     s = str(s)
@@ -19,12 +20,14 @@
     r = int(s, 2) 
     if r - n == 133:
         print(n)'''
+
 '''a = [] 
 for x in range(100, 3001):
     i = int(bin(x)[3:], 2)
     if x - i not in a:
         a.append(x-i)
 print(len(a))'''
+
 #10
 '''for n in range(1000, 10000):
     n = str(n)
@@ -37,6 +40,7 @@ print(len(a))'''
     m = str(max2) + str(max1)
     if m == '1517':
         print(n)'''
+
 '''for n in range(1000, 10000):
     n = str(n)
     s1 = int(n[0]) + int(n[1])
@@ -47,3 +51,20 @@ print(len(a))'''
     s = str(sr) + str(ma)
     if s == '613':
         print(n)'''
+
+# троичная
+def f(x):
+    x3 = ''
+    while x != 0:
+        x3 += str(x % 3)
+        x //= 3
+    return x3[::-1]
+
+
+for n in range(1, 1000):
+    if n % 3 == 0:
+        r = '1' + f(n) + '02'
+    else:
+        r = f(n) + f((n % 3) * 4)
+    if int(r, 3) < 199:
+        print(n)
